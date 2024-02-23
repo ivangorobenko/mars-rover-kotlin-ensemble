cd80a8448527cfaf96d91a00c95f641dc1d80f0e
Bastien David <bastien.david@sundayapp.com>
2024-02-23 14:34:03 +0100

    refactor(forward): use parameterized test

--- src/test/kotlin/MoveForwardTest.kt
+++ src/test/kotlin/MoveForwardTest.kt
@@ -1,34 +1,32 @@
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class MoveForwardTest {
-
-    @Test
-    fun `moves the rover forward when facing north`() {
-        val rover = Rover(Direction.NORTH, Position(1, 7))
+    @ParameterizedTest
+    @MethodSource("moveForwardParameters")
+    fun `moves the rover forward for all directions`(
+        direction: Direction,
+        position: Position,
+        expectedPosition: Position
+    ) {
+        val rover = Rover(direction, position)
         val roverThatMoved = rover.execute(MoveForward())
-        Assertions.assertThat(roverThatMoved).isEqualTo(Rover(Direction.NORTH, Position(1, 6)))
+        Assertions.assertThat(roverThatMoved).isEqualTo(Rover(direction, expectedPosition))
     }
 
-    @Test
-    fun `moves the rover forward when facing south`() {
-        val rover = Rover(Direction.SOUTH, Position(1, 7))
-        val roverThatMoved = rover.execute(MoveForward())
-        Assertions.assertThat(roverThatMoved).isEqualTo(Rover(Direction.SOUTH, Position(1, 8)))
-    }
-
-    @Test
-    fun `moves the rover forward when facing east`() {
-        val rover = Rover(Direction.EAST, Position(1, 7))
-        val roverThatMoved = rover.execute(MoveForward())
-        Assertions.assertThat(roverThatMoved).isEqualTo(Rover(Direction.EAST, Position(2, 7)))
-    }
-
-    @Test
-    fun `moves the rover forward when facing west`() {
-        val rover = Rover(Direction.WEST, Position(1, 7))
-        val roverThatMoved = rover.execute(MoveForward())
-        Assertions.assertThat(roverThatMoved).isEqualTo(Rover(Direction.WEST, Position(0, 7)))
+    companion object {
+        @JvmStatic
+        fun moveForwardParameters(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(Direction.NORTH, Position(1, 7), Position(1, 6)),
+                Arguments.of(Direction.SOUTH, Position(1, 7), Position(1, 8)),
+                Arguments.of(Direction.EAST, Position(1, 7), Position(2, 7)),
+                Arguments.of(Direction.WEST, Position(1, 7), Position(0, 7))
+            )
+        }
     }
 }
 
