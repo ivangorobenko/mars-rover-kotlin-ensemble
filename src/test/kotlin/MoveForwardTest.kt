dd75dbefe9558289d7626a0673c07ad8023e7137
Ivan Gorobenko <ivan.gorobenko@sundayapp.com>
2024-02-23 14:23:21 +0100

    refactor: give position calculation responsibility to Position object

--- src/test/kotlin/MoveForwardTest.kt
+++ src/test/kotlin/MoveForwardTest.kt
@@ -9,20 +9,40 @@ class MoveForwardTest {
         val roverThatMoved = rover.execute(MoveForward())
         Assertions.assertThat(roverThatMoved).isEqualTo(Rover(Direction.NORTH, Position(1, 6)))
     }
+
+    @Test
+    fun `moves the rover forward when facing south`() {
+        val rover = Rover(Direction.SOUTH, Position(1, 7))
+        val roverThatMoved = rover.execute(MoveForward())
+        Assertions.assertThat(roverThatMoved).isEqualTo(Rover(Direction.SOUTH, Position(1, 8)))
+    }
 }
 
 class MoveForward {
-    fun compute(position: Position) = Position(position.x, position.y - 1)
+    fun compute(position: Position, direction: Direction): Position {
+
+        if (direction == Direction.SOUTH) return position.moveSouth()
+        return position.moveNorth()
+    }
 }
 
-data class Position(val x: Int, val y: Int)
+data class Position(val x: Int, val y: Int) {
+    fun moveSouth(): Position {
+        return Position(x, y + 1)
+    }
+
+    fun moveNorth(): Position {
+        return Position(x, y - 1)
+    }
+}
+
 enum class Direction {
     NORTH, SOUTH, EAST, WEST
 }
 
 data class Rover(val direction: Direction, val position: Position) {
     fun execute(moveForward: MoveForward): Rover {
-        return Rover(this.direction, moveForward.compute(position))
+        return Rover(this.direction, moveForward.compute(position, direction))
     }
 
 }
